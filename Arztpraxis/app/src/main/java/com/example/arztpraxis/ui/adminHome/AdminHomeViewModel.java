558e077b6b7c154c0845db931f99039d216bb43b
Cojohnny10 <77165202+Cojohnny10@users.noreply.github.com>
2021-01-09 00:49:45 +0100

    Create AdminHomeViewModel.java

---
+++ Arztpraxis/app/src/main/java/com/example/arztpraxis/ui/adminHome/AdminHomeViewModel.java
@@ -0,0 +1,27 @@
+package com.example.arztpraxis.ui.adminHome;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.arztpraxis.helper.helper;
+import com.example.arztpraxis.model.ScheduleRequest;
+import com.example.arztpraxis.model.Employee;
+import com.example.arztpraxis.model.Patient;
+import com.example.arztpraxis.ws.InfrastructureWebservice;
+
+
+public class AdminHomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+    private MutableLiveData<String[]>mScheduleRequest;
+
+    public AdminHomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Fragment für Terminanfragen");
+
+    }
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
