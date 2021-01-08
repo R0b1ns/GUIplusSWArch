80d5e2afeeee51174200bc94cf813eaa4b8eed17
LordShola <56320972+LordShola@users.noreply.github.com>
2021-01-08 20:12:10 +0100

    HomeFragment works
    HomeFragment doesn't crash on no connection anymore \o/

--- Arztpraxis/app/src/main/java/com/example/arztpraxis/ui/home/HomeViewModel.java
+++ Arztpraxis/app/src/main/java/com/example/arztpraxis/ui/home/HomeViewModel.java
@@ -1,19 +1,103 @@
 package com.example.arztpraxis.ui.home;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.arztpraxis.helper.helper;
+import com.example.arztpraxis.model.HealthInsurance;
+import com.example.arztpraxis.model.Patient;
+import com.example.arztpraxis.model.Person;
+import com.example.arztpraxis.ws.InfrastructureWebservice;
+import com.example.arztpraxis.ws.NoSuchRowException;
+
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private MutableLiveData<String> mName;
+    private MutableLiveData<String> mBirthday;
+    private MutableLiveData<String> mGender;
+    private MutableLiveData<String> mHealthInsurance;
+    private MutableLiveData<String> mHealthInsuranceNumber;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
+        mName = new MutableLiveData<>();
+        mName.setValue("no data found");
+        mBirthday = new MutableLiveData<>();
+        mBirthday.setValue("no data found");
+        mGender = new MutableLiveData<>();
+        mGender.setValue("no data found");
+        mHealthInsurance = new MutableLiveData<>();
+        mHealthInsurance.setValue("no data found");
+        mHealthInsuranceNumber = new MutableLiveData<>();
+        mHealthInsuranceNumber.setValue("no data found");
+
+        new AsyncLoadPerson().execute();
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public LiveData<String> getName() {
+        return mName;
+    }
+
+    public LiveData<String> getBirthday() {
+        return mBirthday;
+    }
+
+    public LiveData<String> getGender() {
+        return mGender;
+    }
+
+    public LiveData<String> getHealthInsurance() {
+        return mHealthInsurance;
+    }
+
+    public LiveData<String> getHealthInsuranceNumber() {
+        return mHealthInsuranceNumber;
+    }
+
+
+
+    private class AsyncLoadPerson extends AsyncTask<Void, Void, Void> {
+        @SuppressLint("WrongThread")
+        @Override
+        protected Void doInBackground(Void...voids) {
+
+            InfrastructureWebservice service = null;
+            service = new InfrastructureWebservice();
+            Person person;
+            Patient patient;
+            HealthInsurance healthInsurance;
+            try {
+                patient = service.getPatient(1);
+                if (patient!=null){
+                    //System.out.println(patient.toString());
+                    person = service.getPerson(patient.getPerson());
+                    //System.out.println(person.toString());
+                    healthInsurance = service.getHealthInsurance(patient.getHealthInsurance());
+                    //System.out.println(healthInsurance.toString());
+                    if (person != null && healthInsurance!=null){
+                        //System.out.println("Status: in person!=null");
+                        mName.postValue(person.getFirstName()+" "+person.getLastName());
+                        mBirthday.postValue(helper.formatDateTime(person.getBirthday(),false));
+                        mGender.postValue(helper.getGender(person.getGender()));
+                        mHealthInsurance.postValue(healthInsurance.getName());
+                        mHealthInsuranceNumber.postValue(String.valueOf(patient.getSSN()));
+                    }
+                }
+            } catch (NoSuchRowException e) {
+                //tvName.setText("Keine Person gefunden");
+                e.printStackTrace();
+            }
+           return null;
+        }
     }
 }
