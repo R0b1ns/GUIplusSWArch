a95c76adbcce69c8fcf2ed4221341fcf532d5f4a
r0b1ns <info@brogit.de>
2021-01-08 21:44:02 +0100

    Konflit gelößt

--- Arztpraxis/app/src/main/java/com/example/arztpraxis/ui/patient/PatientFragment.java
+++ Arztpraxis/app/src/main/java/com/example/arztpraxis/ui/patient/PatientFragment.java
@@ -1,5 +1,6 @@
 package com.example.arztpraxis.ui.patient;
 
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -13,10 +14,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.arztpraxis.R;
+import com.example.arztpraxis.ui.appointment.AppointmentNewFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PatientFragment extends Fragment {
 
     private PatientViewModel mViewModel;
+    private PatientAddFragment patientAddFragment;
 
     public static PatientFragment newInstance() {
         return new PatientFragment();
@@ -25,7 +29,21 @@ public class PatientFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_patient, container, false);
+        View root = inflater.inflate(R.layout.fragment_patient, container, false);
+
+        FloatingActionButton patientAdd = root.findViewById(R.id.patientAdd);
+        patientAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                patientAddFragment = new PatientAddFragment();
+                FragmentTransaction ft = getParentFragmentManager().beginTransaction();
+                ft.replace(R.id.nav_host_fragment, patientAddFragment, "patientAdd");
+                ft.addToBackStack("patientAdd");
+                ft.commit();
+            }
+        });
+
+        return root;
     }
 
     @Override
