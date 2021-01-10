148768c6c59f1c2fda4793603f94fbfa4ae7484b
r0b1ns <info@brogit.de>
2021-01-10 16:23:08 +0100

    Navigation stuff

--- Arztpraxis/app/src/main/java/com/example/arztpraxis/ui/settings/SettingsFragment.java
+++ Arztpraxis/app/src/main/java/com/example/arztpraxis/ui/settings/SettingsFragment.java
@@ -12,10 +12,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+
+import android.widget.TextView;
+
 
 import com.example.arztpraxis.R;
 import com.example.arztpraxis.helper.MyApplication;
